1fd5c43b68cd5553692cff53db2cab4e46ebd7ac
Reka Giday <giday.r@gmail.com>
2018-09-08 23:58:34 +0100

    initial commit

---
+++ src/main/java/reka/reptilerescue/ReptileRescueApplication.java
@@ -0,0 +1,12 @@
+package reka.reptilerescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReptileRescueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReptileRescueApplication.class, args);
+	}
+}
---
+++ src/test/java/reka/reptilerescue/ReptileRescueApplicationTests.java
@@ -0,0 +1,16 @@
+package reka.reptilerescue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReptileRescueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
